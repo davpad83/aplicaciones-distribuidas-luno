95942e8940ccfcb77ba16673b70522e2db0e2790
matiasfavale <matiasfavale@71f9a12d-8279-3fa5-597b-63cab5c32f40>
2011-08-30 00:43:23 +0000

    Initial import - Agrego el proyecto AD-Ej_Clase4_Cliente

---
+++ trunk/AD-Ej_Clase4_Cliente/src/uade/ejercicio/clase4/events/EventHandling.java
@@ -0,0 +1,29 @@
+package uade.ejercicio.clase4.events;
+
+import uade.ejercicio.clase4.beans.DireccionView;
+
+
+
+public class EventHandling {
+
+	
+	
+	public void agregarAlumnoEvent(int legajo, String nombre, String estado) {
+	}
+	
+	public void agregarMateriaEvent(String nombre, String numero) {
+	}
+	
+	public void agregarProfesorEvent(int numeroLegajo, DireccionView direccion) {
+	
+	}
+	
+	public void eliminarAlumnoEvent(int legajo) {
+	}
+	
+	public void eliminarMateriaEvent(String numero) {
+	}
+	
+	public void eliminarProfesorEvent(int numeroLegajo) {
+	}
+}

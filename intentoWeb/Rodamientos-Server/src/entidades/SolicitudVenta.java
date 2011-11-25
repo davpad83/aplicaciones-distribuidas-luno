48b1612631eec8900948ce7770175a039299a400
joacoatt <joacoatt@gmail.com>
2011-11-25 04:16:06 +0000

    Initial import.

---
+++ intentoWeb/Rodamientos-Server/src/entidades/SolicitudVenta.java
@@ -0,0 +1,30 @@
+package entidades;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolicitudVenta implements Serializable
+{
+	private static final long serialVersionUID = 7068820790993609615L;
+	
+	private List<CantidadRodamiento> rodamientos ;
+
+	public SolicitudVenta (){
+		super();
+		this.rodamientos = new ArrayList<CantidadRodamiento>();
+	}
+	
+	public List<CantidadRodamiento> getRodamientos() {
+		return rodamientos;
+	}
+
+	public void setRodamientos(List<CantidadRodamiento> rodamientos) {
+		this.rodamientos = rodamientos;
+	}
+	
+	public void agregarRodamiento (CantidadRodamiento rod) {
+		this.rodamientos.add(rod);
+	}
+	
+}

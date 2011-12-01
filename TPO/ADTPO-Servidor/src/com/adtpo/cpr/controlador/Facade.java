5490e6e4b51a2075690aad8f635a700fe97c3d2d
joacoatt <joacoatt@gmail.com>
2011-12-01 22:58:17 +0000

    creado el facade, agregadas algunas funciones

---
+++ TPO/ADTPO-Servidor/src/com/adtpo/cpr/controlador/Facade.java
@@ -0,0 +1,30 @@
+package com.adtpo.cpr.controlador;
+
+import java.util.ArrayList;
+import com.adtpo.cpr.beans.model.*;
+
+public class Facade {
+
+	//LLamado desde el Bussiness Delegate
+	
+	public Cliente getCliente(int idCliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer obtenerIdCotizacion(ArrayList<ItemRodamiento> rodamientos) {
+		// TODO //busco el ID de la cotizacion para los rodamientos solicitados en una venta
+		return null;
+	}
+
+	public Cotizacion enviarSolicitudDeCotizacion(String nombreDeCotizacion) {
+		return null;// TODO Auto-generated method stub
+	}
+
+	public Factura enviarSolicitudVenta(SolicitudVenta solicitud) {
+		return null;
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
